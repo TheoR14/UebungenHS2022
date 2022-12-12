572b0d7b7774838ec4bf3d8ef38760d622567fab
Theo Reibel <theo.reibel@students.fhnw.ch>
2022-12-12 20:09:33 +0100

    Final Version

--- Uebung09/Uebung9_Aufgabe1.py
+++ Uebung09/Uebung9_Aufgabe1.py
@@ -13,10 +13,12 @@ async def transform1 (lng: float, lat: float):
     r1 = transformer1.transform(lng, lat)
     return f"""ost: {r1[0]}, nord: {r1[1]}"""
 
-@app.get("transform/lv95wgs84")
+@app.get("/transform/lv95wgs84")
 async def transform2 (lng: float, lat: float):
     r2 = transformer2.transform(lng, lat)
     return f"""ost: {r2[0]}, nord: {r2[1]}"""
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001, root_path="/transform")
+    uvicorn.run(app, host="127.0.0.1", port=8003, root_path="/transform") #mit 8003 gemacht, da problem: [Errno 98] error while attempting to bind on address ('127.0.0.1', 8001): address already in use
+
+#   https://vm24.sourcelab.ch/transform/lv95wgs84?lng=2600000&lat=1260000
--- Uebung09/Uebung9_Aufgabe2.py
+++ Uebung09/Uebung9_Aufgabe2.py
@@ -1,25 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import pyproj
 
 g = pyproj.Geod(ellps="WGS84")
 
 app = FastAPI()
 
-@app.get("/")
-async def geodetic (startlong:float, startlat:float, endlong:float, endlat:float):
-    lonlats = g.npts(startlong, startlat, endlong, endlat, )
+@app.get("/geodetic/&")
+async def geodetic (startlong:float, startlat:float, endlong:float, endlat:float, punkte:int):
+    lonlats = g.npts(startlong, startlat, endlong, endlat, punkte) #    punkte ist # Punkte das man will (als Ganzzahl)
     lonlats = [[startlong, startlat]]+ [list(i) for i in lonlats] + [[endlong, endlat]]
-    return f"""{{
-  "type": "Feature",
-  "geometry": {{
-    "type": "Point",
-    "coordinates": {lonlats}
-  }},
-  "properties": {{
-    "attribut": "Beliebige Attribute"
-  }}
-}}"""
+    geojson = f"""{{
+    "type":"Feature",
+    "geometry":{{
+        "type":"LineString",
+        "coordinates":{lonlats}
+    }},
+    "properties": {{
+        "attribut": "Beliebige Attribute"
+    }}
+    }}"""
+    
+    return Response(content=geojson)
+    
+    # mit Response gibt das Problem mit \ und \n bei Zeichenketten niocht mehr
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8002, port_path="/geodetic")
+    uvicorn.run(app, host="127.0.0.1", port=8002)
+
+# https://vm24.sourcelab.ch/geodetic/&?startlong=8.53&startlat=47.37&endlong=-74.00&endlat=40.70&punkte=12
